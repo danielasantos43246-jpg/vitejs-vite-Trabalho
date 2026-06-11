627c8431e6644e04fbb366a4a202f4b691d203ec
danielasantos43246-jpg <danielasantos43246@gmail.com>
2026-06-11 15:06:21 -0300

    adicionei

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,24 +1,15 @@
-function ProductCard({ image, title, price, id, onAddCart }) {
+function  ProductCard() {
   return (
-    <>
-      <div className="product-card">
-        <img src={image} alt={title} />
+    <div className= "ProductCard">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXi2eyLN8zofZzUvbjtPI8lPpwsZHpR95LJQ&s" alt="Produto de barba" />
+      <div className="Product-Info">
+        <h2>Shampoo p/ Barba e Cabelo</h2>
+        <p className="Product-price">R$ 55,96</p>
+        <button className="btn-add">Adicionar ao Carrinho</button>
 
-        <div className="product-info">
-          <h2>{title}</h2>
-          <p className="product-price">R$ {price}</p>
-          <button
-          onClick={() => {
-            onAddCart({ id, title, price, image });
-          }}
-          className="btn-add"
-        >
-          Adicionar ao Carrinho
-        </button>
       </div>
     </div>
-  </>
-);
+  );
 }
 
 export default ProductCard;
