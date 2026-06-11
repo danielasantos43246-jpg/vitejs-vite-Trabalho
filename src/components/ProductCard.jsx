b35d4917645916efe2cf0cb98b2946d3739191bc
danielasantos43246-jpg <danielasantos43246@gmail.com>
2026-06-11 16:45:04 -0300

    adicionei

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,10 @@
-function  ProductCard() {
+function  ProductCard({ name, price, image}) {
   return (
     <div className= "ProductCard">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXi2eyLN8zofZzUvbjtPI8lPpwsZHpR95LJQ&s" alt="Produto de barba" />
+      <img src={image} alt={name}/>
       <div className="Product-Info">
-        <h2>Shampoo p/ Barba e Cabelo</h2>
-        <p className="Product-price">R$ 55,96</p>
+        <h2>{name}</h2>
+        <p className="Product-price">R${price}</p>
         <button className="btn-add">Adicionar ao Carrinho</button>
 
       </div>
